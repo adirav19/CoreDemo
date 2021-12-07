efe1c3f3f82fea04dd3e30cb2ecd26615365b30d
Ri <Ri@DESKTOP-VTBEKBD>
2021-12-07 13:23:09 +0300

    Proje dosyası ekle.

---
+++ BussinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,45 @@
+﻿using BussinessLayer.Abstract;
+using DataAccesLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessLayer.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        EFCategoryRepository eFCategory;
+        public CategoryManager()
+        {
+            eFCategory = new EFCategoryRepository();
+        }
+        public void CategoryAdd(Category category)
+        {
+
+            eFCategory.Insert(category);
+        }
+
+        public void CategoryDelete(Category category)
+        {
+            eFCategory.Delete(category);
+        }
+
+        public void CategoryUpdate(Category category)
+        {
+            eFCategory.Update(category);
+        }
+
+        public Category GetById(int id)
+        {
+            return eFCategory.GetByID(id);
+        }
+
+        public List<Category> GetList()
+        {
+            return eFCategory.GetListAll();
+        }
+    }
+}
---
+++ DataAccesLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccesLayer.Abstract
+{
+   public interface IGenericDal<T> where T : class // bir sınıfa ait tüm değerleri kullanacak demektir
+    {
+
+        void Insert(T t);
+        void Delete(T t);
+        void Update(T t);
+        List<T> GetListAll();
+        T GetByID(int id);
+    }
+}
